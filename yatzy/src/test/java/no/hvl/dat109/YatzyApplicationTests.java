274b62c63ff3cddf59aabae47d1a6a6a8a2c71b1
676627 <142162653+676627@users.noreply.github.com>
2025-02-28 12:26:30 +0100

    create maven project

---
+++ yatzy/src/test/java/no/hvl/dat109/YatzyApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
