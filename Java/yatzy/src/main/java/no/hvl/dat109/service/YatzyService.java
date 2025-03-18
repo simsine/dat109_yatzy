6a4b365e366211023a890c88499ac580a7b2b963
Herman <142395938+h676611@users.noreply.github.com>
2025-03-18 10:20:09 +0100

    laget tomme klasser

---
+++ Java/yatzy/src/main/java/no/hvl/dat109/service/YatzyService.java
@@ -0,0 +1,14 @@
+package no.hvl.dat109.service;
+
+public class YatzyService {
+	
+	
+	/**
+	 * Starter et nytt yatzy spill fra contollerene
+	 */
+	public void startSpill() {
+		//TODO
+	}
+	
+	
+}
