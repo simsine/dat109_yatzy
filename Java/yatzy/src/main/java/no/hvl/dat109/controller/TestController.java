c86edbf156d7fec451fbce52b554dc7cf402a934
Johan <142352311+h180240@users.noreply.github.com>
2025-03-18 11:43:40 +0100

    Update .jsp
    Lagt til getmapping i testController + Endret navn fra tidligereSpillView til spillHistorikkView

--- Java/yatzy/src/main/java/no/hvl/dat109/controller/TestController.java
+++ Java/yatzy/src/main/java/no/hvl/dat109/controller/TestController.java
@@ -10,4 +10,29 @@ public class TestController {
 	public String index() {
 		return "testView";
 	}
+	
+	@GetMapping("/lobby")
+	public String getLobby() {
+		return "lobbyView";
+	}
+	
+	@GetMapping("/innlogging")
+	public String getInnlogging() {
+		return "logginnView";
+	}
+	
+	@GetMapping("/registrering")
+	public String getRegistrering() {
+		return "registrerView";
+	}
+	
+	@GetMapping("/spill")
+	public String getSpill() {
+		return "spillView";
+	}
+	
+	@GetMapping("/spillhistorikk")
+	public String getSpillhistorikk() {
+		return "spillHistorikkView";
+	}
 }
