1ba743fc7ab64d8de1a3daed8070329b10ad683d
Herman <142395938+h676611@users.noreply.github.com>
2025-03-19 10:58:59 +0100

    simsekdi

--- Java/yatzy/src/main/java/no/hvl/dat109/yatzy/YatzySimSpill.java
+++ Java/yatzy/src/main/java/no/hvl/dat109/yatzy/YatzySimSpill.java
@@ -1,5 +1,17 @@
 package no.hvl.dat109.yatzy;
 
+/**
+ * Simulerer et fullstendig yatzyspill
+ */
 public class YatzySimSpill {
+	
+	Poengtabell poengtabell;
 
+	public YatzySimSpill() {
+		poengtabell = new Poengtabell();
+	}
+
+	public void simulerSpill() {
+		
+	}
 }
