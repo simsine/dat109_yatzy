c7d07bf6ef47beba2b232a5f178ebd0dc1798ea3
Herman <142395938+h676611@users.noreply.github.com>
2025-03-19 16:47:30 +0100

    Update Poengtabell.java

--- Java/yatzy/src/main/java/no/hvl/dat109/yatzy/Poengtabell.java
+++ Java/yatzy/src/main/java/no/hvl/dat109/yatzy/Poengtabell.java
@@ -1,30 +1,24 @@
 package no.hvl.dat109.yatzy;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 
 public class Poengtabell {
 
 	private HashMap<PoengType, Integer> poeng;
 	private boolean erYatzyRegistrert;
-	private int rundeNr;
 
 	public Poengtabell() {
 		poeng = new HashMap<PoengType, Integer>();
-		this.setRundeNr(0);
 	}
 
 	/**
 	 * Registrer et resultat for en runde
 	 * 
-	 * @param type  Kombinasjonstype som det skal registres poeng for
+	 * @param type     Kombinasjonstype som det skal registres poeng for
 	 * @param resultat resultatverdien for runden
 	 */
 	public void registrerPoeng(PoengType type, int resultat) {
 		poeng.put(type, resultat);
-		this.setRundeNr(this.getRundeNr() + 1);
 	}
 
 	public int getPoeng(PoengType type) {
@@ -46,15 +40,4 @@ public class Poengtabell {
 	public int getSum() {
 		return this.poeng.values().stream().reduce(0, (acc, x) -> x + acc);
 	}
-
-	public int getRundeNr() {
-		return rundeNr;
-	}
-
-	public void setRundeNr(int rundeNr) {
-		this.rundeNr = rundeNr;
-	}
-
-
-	
 }
