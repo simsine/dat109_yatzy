a90187f296aa04d5ed6e722d0023c40bf9dd8670
Herman <142395938+h676611@users.noreply.github.com>
2025-03-19 16:45:55 +0100

    la til request kommentarer for sim

--- Java/yatzy/src/main/java/no/hvl/dat109/yatzy/Poengtabell.java
+++ Java/yatzy/src/main/java/no/hvl/dat109/yatzy/Poengtabell.java
@@ -1,43 +1,60 @@
 package no.hvl.dat109.yatzy;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
-public class Poengtabell {	
-	
+public class Poengtabell {
+
 	private HashMap<PoengType, Integer> poeng;
 	private boolean erYatzyRegistrert;
-	
-	
+	private int rundeNr;
+
 	public Poengtabell() {
 		poeng = new HashMap<PoengType, Integer>();
+		this.setRundeNr(0);
 	}
-	
+
 	/**
 	 * Registrer et resultat for en runde
-	 * @param rundeNr nummer på runde
+	 * 
+	 * @param type  Kombinasjonstype som det skal registres poeng for
 	 * @param resultat resultatverdien for runden
 	 */
-	public void registrerPoeng(PoengType type, int resultat) { 
+	public void registrerPoeng(PoengType type, int resultat) {
 		poeng.put(type, resultat);
+		this.setRundeNr(this.getRundeNr() + 1);
 	}
 
 	public int getPoeng(PoengType type) {
 		return poeng.get(type);
 	}
-	
+
 	public HashMap<PoengType, Integer> getAllePoeng() {
 		return poeng;
 	}
-	
+
 	public boolean getErYatzyRegistrert() {
 		return erYatzyRegistrert;
 	}
-	
+
 	public void setErYatzyRegistrert(boolean yatzyRegistrert) {
 		this.erYatzyRegistrert = yatzyRegistrert;
 	}
-	
+
 	public int getSum() {
-		return getAllePoeng().values().stream().reduce(0, (acc, x) -> x + acc);
+		return this.poeng.values().stream().reduce(0, (acc, x) -> x + acc);
 	}
+
+	public int getRundeNr() {
+		return rundeNr;
+	}
+
+	public void setRundeNr(int rundeNr) {
+		this.rundeNr = rundeNr;
+	}
+
+
+	
 }
--- Java/yatzy/src/main/java/no/hvl/dat109/yatzy/YatzySimSpill.java
+++ Java/yatzy/src/main/java/no/hvl/dat109/yatzy/YatzySimSpill.java
@@ -31,21 +31,36 @@ public class YatzySimSpill {
 		System.out.println("Utfører simulering av fullstendig spill\n");
 		for (PoengType type : PoengType.values()) {
 			System.out.println("Runde: " + type.toString());
+			
+			//POST /trill {tom liste} 1
 			List<Integer> beholdte = new ArrayList<Integer>();
 			beholdte = yatzyService.spillTrekk(beholdte);
 			System.out.println("Spiller kastet følgende terninger: " + beholdte.toString());
+			//return terningverdier
+			
+			//bruker velger terninger
 			beholdte.removeLast();
 			beholdte.removeLast();
 			System.out.println("Spiller valgte følgende terninger å beholde: " + beholdte.toString());
+			
+			//POST /trill {beholdte terninger} 2
 			beholdte = yatzyService.spillTrekk(beholdte);
 			System.out.println("Spiller kastet følgende terninger: " + beholdte.toString());
+			//return terningverdier
+
+			//bruker velger terninger
 			beholdte.removeLast();
 			beholdte.removeLast();
 			System.out.println("Spiller valgte følgende terninger å beholde: " + beholdte.toString());
+			
+			//POST /trill {beholdte terninger} 3
 			beholdte = yatzyService.spillTrekk(beholdte);
 			System.out.println("Spiller kastet følgende terninger: " + beholdte.toString());
+
 			int poeng = yatzyService.registrerPoeng(poengtabell, type, beholdte);
 			System.out.println("Spiller fikk " + type.toString() + " resultat: " + poeng);
+			//return poeng
+
 		}
 		System.out.println("\nSimulering er ferdig, sum er: " + poengtabell.getSum());
 		System.out.println("---------------------------------------------------");
