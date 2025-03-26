b4eb46fad2b6f6aeb8281230c16b2412fa394078
Herman <142395938+h676611@users.noreply.github.com>
2025-03-26 12:28:17 +0100

    Update PoengtabellRepo.java

--- Java/yatzy/src/main/java/no/hvl/dat109/repo/PoengtabellRepo.java
+++ Java/yatzy/src/main/java/no/hvl/dat109/repo/PoengtabellRepo.java
@@ -3,6 +3,8 @@ package no.hvl.dat109.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import no.hvl.dat109.entity.Poengtabell;
@@ -10,5 +12,12 @@ import no.hvl.dat109.entity.PoengtabellId;
 
 @Repository
 public interface PoengtabellRepo extends JpaRepository<Poengtabell, PoengtabellId> {
+	
+	@Query("SELECT p FROM Poengtabell p WHERE p.id.brukernavn = :brukernavn")
+	List<Poengtabell> findByBrukernavn(@Param("brukernavn") String brukernavn);
+	
+	@Query("Select p FROM Poengtabell p WHERE p.id.brukernavn = :brukernavn"
+			+ "AND p.id.spillnr = :spillnr")
+	Poengtabell findByBrukernavnAndSpillnr(@Param("brukernavn") String brukernavn, @Param("spillnr") int spillnr);
 
 }
