a77c8c2bb7bb3952ffd841cca7c4cee5abf23869
676627 <142162653+676627@users.noreply.github.com>
2025-03-26 12:18:39 +0100

    update and move SQL

---
+++ Java/yatzy/src/main/java/YatzyDB.sql
@@ -0,0 +1,30 @@
+DROP SCHEMA IF EXISTS yatzy CASCADE;
+CREATE SCHEMA yatzy;
+SET search_path TO yatzy;
+
+CREATE TABLE spill (
+    spillnr SERIAL PRIMARY KEY
+);
+
+CREATE TABLE spiller (
+    brukernavn varchar(255) PRIMARY KEY,
+    email VARCHAR(255),
+    fornavn VARCHAR(255),
+    etternavn VARCHAR(255),
+    hashetpassord VARCHAR(255),
+    salt VARCHAR(255)
+);
+
+CREATE TABLE poengtabell (
+    brukernavn VARCHAR(255),
+    spillnr INTEGER,
+    poeng JSONB,
+    PRIMARY KEY (brukernavn, spillnr),
+    FOREIGN KEY (brukernavn) REFERENCES spiller(brukernavn)
+);
+
+INSERT INTO spiller(brukernavn, email, fornavn, etternavn, hashetpassord, salt) VALUES 
+('XFaze', 'jada@gugel.no', 'Simeon', 'Strømsfjes', 'hei', 'NaCL'),
+('Jogustauhan', 'nehei@jahu.net', 'Auguhan', 'Staavik', 'ikke', 'SaLT'),
+('MisterMan', 'heman@muskel.com', 'Herdahl', 'Manberg', 'oiasjd', 'HCl'),
+('Lajus', 'l@sse.no', 'Labbe', 'Uggus', 'neitakk', 'OIAJSDOIJASDIOJ1234!');
