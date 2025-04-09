e23523c74766ca5b5fc5927acf38bb6532bca345
Herman <142395938+h676611@users.noreply.github.com>
2025-04-09 17:11:43 +0200

    man kan ikke se andre sin spillhistorikk

--- Java/yatzy/src/main/java/no/hvl/dat109/controller/SpillhistorikkController.java
+++ Java/yatzy/src/main/java/no/hvl/dat109/controller/SpillhistorikkController.java
@@ -1,7 +1,10 @@
 package no.hvl.dat109.controller;
 
+import java.net.http.HttpClient;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,8 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.servlet.http.HttpSession;
 import no.hvl.dat109.entity.Spill;
+import no.hvl.dat109.entity.Spiller;
 import no.hvl.dat109.service.SpillService;
+import no.hvl.dat109.service.SpillerService;
 import no.hvl.dat109.service.SpillgjennomgangService;
 
 @Controller
@@ -22,9 +28,10 @@ public class SpillhistorikkController {
 	
 	@Autowired
 	private SpillService spillService;
+
 	
 	@GetMapping("/spillhistorikk/{brukernavn}")
-	public String getSpillhistorikk(@PathVariable("brukernavn") String brukernavn, Model model) {
+	public String getSpillhistorikk(@PathVariable("brukernavn") String brukernavn, Model model, HttpSession session) {
 		List<Spill> aktivespill = new ArrayList<>(spillService.hentAlleIkkeFerdigeSpillForSpiller(brukernavn));
 		aktivespill.sort((o1, o2) -> o1.getTidopprettet().compareTo(o2.getTidopprettet()));
 		List<Spill> avsluttedespill = new ArrayList<>(spillService.hentAlleFerdigeSpillForSpiller(brukernavn));
@@ -32,6 +39,10 @@ public class SpillhistorikkController {
 		model.addAttribute("aktivespill", aktivespill);
 		model.addAttribute("avsluttedespill", avsluttedespill);
 		model.addAttribute("brukernavn", brukernavn);
+		Spiller spiller = (Spiller) session.getAttribute("spiller");
+		if (!spiller.getBrukernavn().equals(brukernavn)) {
+			return "redirect:/lobby";
+		}
 		
 		return "spillHistorikkView";
 	}
